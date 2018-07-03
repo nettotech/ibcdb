d0f4d403f226b73acb9d832555eb1d025e8aca1c
Mario Netto <mario@mario.eti.br>
2018-07-03 12:27:40 -0300

    Limpeza Arquivos

--- api/app/controllers/tecnology.js
+++
@@ -1,7 +0,0 @@
-module.exports.tecnology = function(application, req, res, next){
-
-    var connection = application.config.dbConnection;
-    var TecnologyDAO = new application.app.models.TecnologyDAO(connection);
-
-    TecnologyDAO.listar(req, res, next);
-}
